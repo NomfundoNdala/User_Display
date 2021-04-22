2d4613fe268eb1119dcb8b5ba010315db4f2c37d
academy_learner <academy_learner@academy-learners-MacBook-Air-5.local>
2021-04-22 09:36:45 +0200

    search button ui

--- clientUI/src/app/app-routing.module.ts
+++ clientUI/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "user", component: UsersComponent },
+  { path: "home", component: HomeComponent },
+  { path: "**", component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
